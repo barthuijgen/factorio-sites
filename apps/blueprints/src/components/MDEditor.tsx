c7c5927f6674aa407a397aa2c70d389121cb35b2
vvav3 <xandgor@gmail.com>
2021-04-01 23:21:59 +0300

    Added markdown editor

---
+++ apps/blueprints/src/components/MDEditor.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styled from "@emotion/styled";
+import SimpleMDE, { SimpleMDEEditorProps } from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+
+const StyledSimpleMDE = styled(SimpleMDE)`
+  .editor-toolbar {
+    button.active,
+    button:hover {
+      background: #5d5d5d;
+    }
+
+    &.fullscreen {
+      background: #414040;
+    }
+  }
+
+  .editor-preview {
+    background: #414040;
+  }
+
+  .CodeMirror {
+    background: #414040;
+    color: white;
+  }
+
+  .CodeMirror-cursor {
+    border-color: white;
+  }
+
+  .EasyMDEContainer .CodeMirror-fullscreen {
+    border-left: none !important;
+    border-bottom: none !important;
+  }
+`;
+
+export const MDEditor: React.FC<SimpleMDEEditorProps> = (props) => {
+  return (
+    <StyledSimpleMDE options={{ spellChecker: false, sideBySideFullscreen: false }} {...props} />
+  );
+};
