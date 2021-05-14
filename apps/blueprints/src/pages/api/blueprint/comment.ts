e93d131f755c87f2fec6595fe3998d0228238496
Bart <barthuijgen@users.noreply.github.com>
2021-05-14 23:42:11 +0200

    feat: added deleting of comments

--- apps/blueprints/src/pages/api/blueprint/comment.ts
+++ apps/blueprints/src/pages/api/blueprint/comment.ts
@@ -1,20 +1,33 @@
 import { apiHandler } from "../../../utils/api-handler";
-import { createComment } from "@factorio-sites/database";
+import { createComment, deleteComment } from "@factorio-sites/database";
 
 const handler = apiHandler(async (req, res, { session }) => {
-  if (req.method !== "POST") return res.status(400).json({ error: "method must be POST" });
+  if (req.method === "POST") {
+    if (!session) {
+      return res.status(401).json({ status: "Not authenticated" });
+    }
 
-  if (!session) {
-    return res.status(401).json({ status: "Not authenticated" });
+    const { body, blueprint_page_id } = req.body;
+    console.log({ body, blueprint_page_id });
+
+    const result = await createComment(blueprint_page_id, session.user, body);
+    console.log(result);
+
+    res.status(200).json({ status: "Comment submitted" });
+  } else if (req.method === "DELETE") {
+    if (!session) {
+      return res.status(401).json({ status: "Not authenticated" });
+    }
+
+    if (session.user.role !== "moderator" && session.user.role !== "admin") {
+      return res.status(401).json({ status: "Not authenticated" });
+    }
+    const { comment_id } = req.body;
+    await deleteComment(comment_id);
+    res.status(200).json({ status: "Comment deleted" });
+  } else {
+    return res.status(400).json({ error: `Unsupported method ${req.method}` });
   }
-
-  const { body, blueprint_page_id } = req.body;
-  console.log({ body, blueprint_page_id });
-
-  const result = await createComment(blueprint_page_id, session.user, body);
-  console.log(result);
-
-  res.status(200).json({ status: "Comment submitted" });
 });
 
 export default handler;
