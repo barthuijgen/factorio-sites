6668ee727ecf96ad68ed10c2182d253b4e40326d
Bart Huijgen <barthuijgen@users.noreply.github.com>
2022-03-12 22:10:20 +0100

    Add cors for factorio.tools to gcp bucket

--- apps/blueprints/src/pages/user/favorites.tsx
+++ apps/blueprints/src/pages/user/favorites.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { NextPage } from "next";
 import { SimpleGrid, Box, Text } from "@chakra-ui/react";
 import { getUserFavoriteBlueprintPages } from "@factorio-sites/database";
