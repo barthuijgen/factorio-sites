60f0001d32008cd7b7aa53e91927be80b4db526f
Bart Huijgen <barthuijgen@users.noreply.github.com>
2022-11-22 17:56:46 +0100

    fix: have primsa use default env value

--- libs/database/src/lib/postgres/database.ts
+++ libs/database/src/lib/postgres/database.ts
@@ -1,12 +1,7 @@
 import { PrismaClient } from "@prisma/client";
-import { getEnv } from "../env.util";
-
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const url = getEnv("DATABASE_URL")!;
 
 export const prisma = new PrismaClient({
   log: ["warn", "error"],
-  datasources: { db: { url } },
 });
 
 export const init = async () => {
